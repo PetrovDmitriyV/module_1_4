40ffdf38b25064fa0c1a70980be560e864f04ba9
PetrovDmitriyV <phanl2@mail.ru>
2024-09-03 18:58:16 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Введите символы ')
+print("Вы ввели ", (len(my_string)), "символов")
+print("Верзний регистр: ", my_string.upper())
+print("Нижний регистр: ", my_string.lower())
+print("Без пробелов: ", my_string.replace(" ", ""))
+print("Первый символ: ", my_string[0])
+print("Последний символ: ", my_string[-1])
